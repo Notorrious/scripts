3c35a185e47ad2afc58ff6f22e534c4c8016cece
Notorrious <99129576+Notorrious@users.noreply.github.com>
2022-02-13 17:42:53 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,5 @@
-if game.GameId == 8592863835 then
-  a = "https://raw.githubusercontent.com/Notorrious/Magnet_Simulator_2/main/Magnet_Simulator_2" -- Magnet Simulator
+if game.PlaceId == 8592863835 then
+  local a = "https://raw.githubusercontent.com/Notorrious/Magnet_Simulator_2/main/Magnet_Simulator_2" -- Magnet Simulator
 end
+
 loadstring(game:HttpGet(a))()
