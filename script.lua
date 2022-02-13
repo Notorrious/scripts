e0effed0e5a34fe85dee5fe538c844685e2fe20a
Notorrious <99129576+Notorrious@users.noreply.github.com>
2022-02-13 18:03:03 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,7 @@
 if game.GameId == 8592863835 then
-        a = "https://raw.githubusercontent.com/Notorrious/Magnet_Simulator_2/main/Magnet_Simulator_2" -- Magnet Simulator
-elseif game.PlaceId == 8357510970 then
-        a = "https://raw.githubusercontent.com/uzu01/lua/main/games/Anime%20Punching%20Simulator.lua" -- Anime Punching Simulator
+	a = "https://raw.githubusercontent.com/Notorrious/Magnet_Simulator_2/main/Magnet_Simulator_2" -- Magnet Simulator 2
+elseif
+    print("Game not supported yet!")
 end
 
-loadstring(game:HttpGet(a,true))()
+loadstring(game:HttpGet(a))()
