c459d306a1dff3977c2be6582625321b2892c2d3
Notorrious <99129576+Notorrious@users.noreply.github.com>
2022-02-13 20:58:36 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -4,10 +4,6 @@ local games = {
     [8592863835] = "Magnet_Simulator_2",
 }
 
-for i,v in next, games do
-    games[i] = table.concat(v:split(' '), '_')
-end
-
 local name = games[game.PlaceId] or games[game.GameId]
 
-return loadstring(game:HttpGet(URL .. "/" .. (name) .. , true))()
+loadstring(game:HttpGet(URL .. "/" .. (name) .. , true))()
