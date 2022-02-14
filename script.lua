17871700a0c87fe806a6ffb05475f614a9d064ef
Notorrious <99129576+Notorrious@users.noreply.github.com>
2022-02-14 16:52:28 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,9 @@
+local abc = "nui"
 if game.GameId == 8592863835 then
-    a = "https://raw.githubusercontent.com/Notorrious/Magnet_Simulator_2/main/Magnet_Simulator_2" -- Magnet Simulator
+    local abc = "https://raw.githubusercontent.com/Notorrious/Magnet_Simulator_2/main/Magnet_Simulator_2" -- Magnet Simulator
 elseif game.PlaceId == 8357510970 then
-    a = "https://raw.githubusercontent.com/uzu01/lua/main/games/Anime%20Punching%20Simulator.lua" -- Anime Punching Simulator
+    local abc = "https://raw.githubusercontent.com/uzu01/lua/main/games/Anime%20Punching%20Simulator.lua" -- Anime Punching Simulator
 end
 
-loadstring(game:HttpGet(a,true))()
+print(a)
+return loadstring(game:HttpGet(abc))()
